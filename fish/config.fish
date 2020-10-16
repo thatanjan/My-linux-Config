4d912189ca8b48eb6e16ead5cc6e6086b732c9f2
Anjan18 <anjanbiber@gmail.com>
2020-10-17 00:40:38 +0600

    polybar has been added

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+alias red='redshift -b'
+alias i3='nvim ~/.config/i3/config'
+alias ali='nvim ~/.config/fish/config.fish'
+alias gb='cd ~/git_projects/good_buy && nvim src'
+alias gbs='cd ~/git_projects/good_buy && yarn start'
+alias v='nvim'
+alias po='sudo poweroff'
+alias rbt='sudo reboot'
+alias nvcon='nvim ~/.config/nvim/init.vim'
+alias paci='sudo pacman -S'
+alias pacr='sudo pacman -Rns'
+alias pacu='sudo pacman -Syu'
+alias picoc='nvim ~/.config/picom/picom.conf'
+alias alac='nvim ~/.config/alacritty/alacritty.yml'
+
+
+set PATH /home/anjan/.yarn/bin $PATH
